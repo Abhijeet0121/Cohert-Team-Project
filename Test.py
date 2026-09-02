c783ebc57673caf27bdb8f08f6e983890a930c4e
RajSardar <Raj.Sardar@ACCEL-DIGITAL.COM>
2026-09-02 17:06:47 -0400

    Add Python programs

---
+++ Test.py
@@ -0,0 +1,7 @@
+print("Hello my name is Raj Sardar")
+print("This is my first Python Program.")
+
+print("I am learning Python programming language.")
+print("I am enjoying the learning process.")
+
+print("I will continue to practice and improve my skills.")
